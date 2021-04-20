faab8dfd851ebab6fe633e10a4957208be72b6d3
Prakhar Mangal <pmangal@deqode.com>
2021-04-20 21:09:04 +0530

    test case updated

--- src/users/users.controller.spec.ts
+++ src/users/users.controller.spec.ts
@@ -1,12 +1,15 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
+import { ConfigModule } from './../config/config.module';
+import { DynamoDBDataMapperModule } from './../mapper/dynamodb-mapper.module';
 
 describe('UsersController', () => {
   let controller: UsersController;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [ConfigModule, DynamoDBDataMapperModule],
       controllers: [UsersController],
       providers: [UsersService],
     }).compile();
